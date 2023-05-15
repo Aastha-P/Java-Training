f286494ae56f3cc9eae6240c6f450298e61756b0
Aastha Patel <114515584+Aastha-P@users.noreply.github.com>
2023-05-16 00:41:33 +0530

    Update FirstServletExample.java

--- Tomcat Server/FirstServletExample.java/FirstServletExample.java
+++ Tomcat Server/FirstServletExample.java/FirstServletExample.java
@@ -1,1 +1,57 @@
+package com.servin;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.Servlet;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+public class FirstServletExample implements Servlet {
+	ServletConfig config=null;
+	
+	@Override
+	public void init(ServletConfig req) throws ServletException {
+		// TODO Auto-generated method stub
+		this.config=config;
+		System.out.println("Servlet is initialized.");
+}
+
+	@Override
+	public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		res.setContentType("text/html");
+		PrintWriter out=res.getWriter();
+		out.print("<html><body>");
+	    out.print("<b>Servlet by implementing Servlet Interface.</b>");
+	    out.print("</body></html>");
+	    out.print("<a href=\"index1.html\">Home Page</a>");
+	}
+	
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+		System.out.println("Servlet is destroyed.");
+		
+	}
+
+	@Override
+	public ServletConfig getServletConfig() {
+		// TODO Auto-generated method stub
+		return config;
+	}
+
+	@Override
+	public String getServletInfo() {
+		// TODO Auto-generated method stub
+		return "my servlet 9.0";
+	}
+
+	
+	
+	
+
+}
 
