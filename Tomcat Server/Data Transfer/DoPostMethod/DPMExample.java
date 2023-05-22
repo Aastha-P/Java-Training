a79ca6f225addf61458b633272156ce082a1dc49
Aastha Patel <114515584+Aastha-P@users.noreply.github.com>
2023-05-22 21:34:02 +0530

    Do Post Method Example

--- Tomcat Server/Data Transfer/DoPostMethod/DPMExample.java
+++ Tomcat Server/Data Transfer/DoPostMethod/DPMExample.java
@@ -1,1 +1,32 @@
+package com.serv;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class PostMethodExample extends HttpServlet {
+	protected void doPost(HttpServletRequest req,HttpServletResponse res)throws IOException{
+		System.out.println("DoPost Method");
+		
+		res.setContentType("text/html");
+		PrintWriter w=res.getWriter();
+		
+		String name=req.getParameter("uname");
+		int age=Integer.parseInt(req.getParameter("age"));
+		
+		w.print("<html><body>");
+		w.print("<h1><b>This is Servlet Page.</b></h1>");
+		w.print("<h2><b>User name is: "+name+"</b></h2>");
+		w.print("<h2><b>Age is: "+age+"</b></h2>");
+		w.print("</body></html>");
+	}
+	
+	public void destroy() {
+		System.out.println("DoGet is destroyed.");
+	}
+
+}
 
